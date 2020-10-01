88a9b1b6e382a34718f2743adb4451ef1badd574
vagrant <vagrant@vagrant.vm>
2020-10-01 00:55:49 +0000

    finished and debugged calculator

--- calculator.py
+++ calculator.py
@@ -13,22 +13,27 @@ while calculator == True:
         calculator = False
     else:
         a = int(token[1])
-        b = int(token[2])
         if token[0] == '+':
+            b = int(token[2])
             x = add(a, b)
         elif token[0] == '-':
+            b = int(token[2])
             x = subtract(a,b)
         elif token[0] == '*':
+            b = int(token[2])
             x = multiply(a,b)
         elif token[0] == '/':
+            b = int(token[2])
             x = divide(a,b)
         elif token[0] == 'square':
-            x = square(a,b)
+            x = square(a)
         elif token[0] == 'cube':
-            x = cube(a,b)
+            x = cube(a)
         elif token[0] == 'pow':
+            b = int(token[2])
             x = power(a,b)
         elif token[0] == 'mod':
+            b = int(token[2])
             x = mod(a,b)
         print(x)
 
